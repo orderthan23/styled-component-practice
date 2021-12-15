380fa7bcd0657ebc1d970a7282aa52bd6218eaac
jonghyunKo <skygg@naver.com>
2021-12-15 21:42:25 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import styled from "styled-components";
 
 //back-tik 사이에 들어가는 모든 문법은 css 여야함
 //styled 뒤에는 html의 태그종류만 입력가능
+
+// 재사용 가능한 style 컴포넌트 만들기
 const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${props => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+//이전 Box 스타일 상속
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
+
 
 const Text = styled.span`
     color : white;
@@ -26,10 +28,10 @@ const Text = styled.span`
 function App() {
     return (
         <Father>
-            <BoxOne>
+            <Box bgColor="teal">
                 <Text>Hello</Text>
-            </BoxOne>
-            <BoxTwo/>
+            </Box>
+            <Circle bgColor="tomato"/>
         </Father>
     );
 }
